74c3929aa6f782147a1eb31228d26714f0b2c79f
AllaZaharova <88785319+AllaZaharova@users.noreply.github.com>
2023-06-19 22:27:39 +0300

    partial complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,24 @@
-import gulp from "gulp";
-import plumber from "gulp-plumber";
-import less from "gulp-less";
-import postcss from "gulp-postcss";
-import autoprefixer from "autoprefixer";
-import csso from "postcss-csso";
-import rename from "gulp-rename";
-import terser from "gulp-terser";
-import squoosh from "gulp-libsquoosh";
-import svgo from "gulp-svgmin";
-import svgstore from "gulp-svgstore";
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import less from 'gulp-less';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import browser from 'browser-sync';
 import minify from "gulp-htmlmin";
-import { deleteAsync } from "del";
-import browser from "browser-sync";
+import svgo from "gulp-svgmin";
 
 // Styles
 
 export const styles = () => {
-  return gulp
-    .src("source/less/style.less", {
-      sourcemaps: true,
-    })
+  return gulp.src('source/less/style.less', { sourcemaps: true })
     .pipe(plumber())
     .pipe(less())
-    .pipe(postcss([autoprefixer(), csso()]))
-    .pipe(rename("style.min.css"))
-    .pipe(
-      gulp.dest("build/css", {
-        sourcemaps: ".",
-      })
-    )
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
-};
+}
 
 // HTML
 
@@ -45,37 +33,19 @@ const html = () => {
     .pipe(gulp.dest("build"));
 };
 
-// Scripts
+// Server
 
-const scripts = () => {
-  return gulp.src("source/js/*.js").pipe(terser()).pipe(gulp.dest("build/js"));
-};
-
-// Images
-
-const optimizeImages = () => {
-  return gulp
-    .src("source/img/**/*.{png,jpg}")
-    .pipe(squoosh())
-    .pipe(gulp.dest("build/img"));
-};
-
-const copyImages = () => {
-  return gulp.src("source/img/**/*.{png,jpg}").pipe(gulp.dest("build/img"));
-};
-
-// WebP
-
-const createWebp = () => {
-  return gulp
-    .src("source/img/**/*.{png,jpg}")
-    .pipe(
-      squoosh({
-        webp: {},
-      })
-    )
-    .pipe(gulp.dest("build/img"));
-};
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
 
 // SVG
 
@@ -98,66 +68,20 @@ export const sprite = () => {
     .pipe(gulp.dest("build/img"));
 };
 
-// Copy
-
-const copy = (done) => {
-  gulp
-    .src(["source/fonts/*.{woff2,woff}"], {
-      base: "source",
-    })
-    .pipe(gulp.dest("build"));
-  done();
-};
-
-// Clean
-
-const clean = () => {
-  return deleteAsync("build");
-};
-
-// Server
-
-const server = (done) => {
-  browser.init({
-    server: {
-      baseDir: "build",
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
-};
-
-// Reload
-
-const reload = (done) => {
-  browser.reload();
-  done();
-};
-
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series(styles));
-  gulp.watch("source/*.html", gulp.series(html, reload));
-};
+  gulp.watch('source/less/**/*.less', gulp.series(styles));
+  gulp.watch('source/*.html').on('change', browser.reload);
+}
+
 
 // Build
 
 export const build = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(styles, html, scripts, svg, sprite, createWebp)
+  gulp.parallel(styles, html, svg)
 );
 
-// Default
-
 export default gulp.series(
-  clean,
-  copy,
-  copyImages,
-  gulp.parallel(styles, html, scripts, svg, sprite, createWebp),
-  gulp.series(server, watcher)
+  styles, server, watcher
 );
